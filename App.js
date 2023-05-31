98e161706fa8d185a5a8880763c170d3c8375f28
durga1214cvr <123532799+durga1214cvr@users.noreply.github.com>
2023-05-31 06:45:39 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Firstcomp from './components/classcomp'
+import Seconcomp from './components/functionalcomp'
+import Thirdcomp from './components/TimeusingFc'
 
 function App() {
   return (
     <div className="App">
       <h3>This is in the React application</h3>
+      <Firstcomp/>
+      <Seconcomp/>
+      <Thirdcomp/>
     </div>
   );
 }
